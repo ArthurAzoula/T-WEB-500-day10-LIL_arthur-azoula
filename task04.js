e6d8a660673b982df62dfbcd7f1c6ee6dcf89f2e
Arthur Azoula <arthur.azoula@epitech.eu>
2023-10-06 16:19:40 +0200

    end of task04

--- task04.js
+++ task04.js
@@ -1,7 +1,113 @@
+// Error codes
+const TYPE_NOT_ENOUGHT_CHARACTERS = 1;
+const TYPE_TOO_MANY_CHARACTERS = 2;
+const TYPE_NON_ALPHABETICAL_CHARACTERS = 3;
+
+const BRAND_NOT_ENOUGHT_CHARACTERS = 4;
+const BRAND_TOO_MANY_CHARACTERS = 5;
+const BRAND_INVALID_CHARACTERS = 6;
+
 const form = document.querySelector('form');
 const type = document.querySelector('#type');
 const brand = document.querySelector('#brand');
 const resultat = document.querySelector('#resultat');
+const ultype = document.querySelector('#check_type');
+const ulbrand = document.querySelector('#check_brand');
+
+type.addEventListener('keyup', (e) => {
+    let typeValue = e.currentTarget.value;
+    typeValue = typeValue.trim();
+    if (typeValue.length > 0) {
+        ultype.classList.remove('d-none');
+    } else {
+        ultype.classList.add('d-none');
+    }
+    const typeError = checkType(typeValue);
+    if (typeError.includes(TYPE_NOT_ENOUGHT_CHARACTERS)) {
+        ultype.children[0].classList.add('text-danger');
+        ultype.children[0].classList.remove('text-success')
+    } else {
+        ultype.children[0].classList.remove('text-danger');
+        ultype.children[0].classList.add('text-success');
+    }
+    if (typeError.includes(TYPE_TOO_MANY_CHARACTERS)) {
+        ultype.children[1].classList.add('text-danger');
+        ultype.children[1].classList.remove('text-success');
+    } else {
+        ultype.children[1].classList.remove('text-danger');
+        ultype.children[1].classList.add('text-success');
+    }
+    if (typeError.includes(TYPE_NON_ALPHABETICAL_CHARACTERS)) {
+        ultype.children[2].classList.add('text-danger');
+        ultype.children[2].classList.remove('text-success');
+    } else {
+        ultype.children[2].classList.remove('text-danger');
+        ultype.children[2].classList.add('text-success');
+    }
+
+});
+
+brand.addEventListener('keyup', (e) => {
+    let brandValue = e.currentTarget.value;
+    brandValue = brandValue.trim();
+    if (brandValue.length > 0) {
+        ulbrand.classList.remove('d-none');
+    } else {
+        ulbrand.classList.add('d-none');
+    }
+    const brandError = checkBrand(brandValue);
+    if (brandError.includes(BRAND_NOT_ENOUGHT_CHARACTERS)) {
+        ulbrand.children[0].classList.add('text-danger');
+        ulbrand.children[0].classList.remove('text-success');
+    } else {
+        ulbrand.children[0].classList.remove('text-danger');
+        ulbrand.children[0].classList.add('text-success');
+    }
+    if (brandError.includes(BRAND_TOO_MANY_CHARACTERS)) {
+        ulbrand.children[1].classList.add('text-danger');
+        ulbrand.children[1].classList.remove('text-sucess');
+    } else {
+        ulbrand.children[1].classList.remove('text-danger');
+        ulbrand.children[1].classList.add('text-success');
+    }
+    if (brandError.includes(BRAND_INVALID_CHARACTERS)) {
+        ulbrand.children[2].classList.add('text-danger');
+        ulbrand.children[2].classList.remove('text-success');
+    } else {
+        ulbrand.children[2].classList.remove('text-danger');
+        ulbrand.children[2].classList.add('text-success');
+    }
+
+    
+});
+
+
+// Function to check if the "type" input is valid
+const checkType = (typeValue) => {
+    resErrors = [];
+    if (typeValue.length < 3) {
+         resErrors.push(TYPE_NOT_ENOUGHT_CHARACTERS);
+    } else if (typeValue.length > 10) {
+        resErrors.push(TYPE_TOO_MANY_CHARACTERS);
+    } else if (!/^[a-zA-Z\-]+$/.test(typeValue)) {
+        resErrors.push(TYPE_NON_ALPHABETICAL_CHARACTERS);
+    }
+    return resErrors; // Type is valid
+};
+
+// Function to check if the "brand" input is valid
+const checkBrand = (brandValue) => {
+    resErrors = [];
+    if (brandValue.length < 2) {
+        resErrors.push(BRAND_NOT_ENOUGHT_CHARACTERS);
+    } else if (brandValue.length > 20) {
+        resErrors.push(BRAND_TOO_MANY_CHARACTERS);
+    } else if (!/^[a-zA-Z0-9\-&]+$/.test(brandValue)) {
+        resErrors.push(BRAND_INVALID_CHARACTERS);
+    }
+    return resErrors; 
+};
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,18 +119,22 @@ form.addEventListener('submit', (e) => {
     })
         .then(response => response.json())
         .then(result => {
-            console.log(result)
             if (result.success) {
-                resultat.innerHTML = 'Created successfully';
+                resultat.textContent = 'Your data is valid';
                 resultat.classList.remove('alert-danger');
                 resultat.classList.add('alert-success');
                 resultat.classList.add('text-white');
+                brand.value = '';
+                type.value = '';
+                ulbrand.classList.add('d-none');
+                ultype.classList.add('d-none');
             } else {
-                resultat.innerHTML = 'Error creating';
+                resultat.textContent = result.error;
                 resultat.classList.remove('alert-success');
                 resultat.classList.add('alert-danger');
                 resultat.classList.add('text-white');
-            }
+            } 
 
     })
 });
+
--- task04.php
+++ task04.php
@@ -1,27 +1,87 @@
 <?php
 
-
 // Include the database connection file
 require_once 'db/connection.php';
 
-// Check if GET data for 'type' and 'brand' exists
-if (isset($_GET['type']) && isset($_GET['brand'])) {
-    // Create query
-    $query = <<<SQL
-        INSERT INTO ajax_products.products (`type`, `brand`, `price`)
-        VALUES (?, ?, ?)
-    SQL;
-
-    // Prepare and execute query
-    $stmt = $conn->prepare($query);
-    $result = $stmt->execute([$_GET['type'], $_GET['brand'], rand(50,200)]);
-
-    // Check result
-    if ($result) {
-        echo json_encode(['success' => true]); // Return 'success' as a response
-    } else {
-        echo json_encode(['success' => false]); // Return 'error' as a response
-    }
-} else {
-    echo json_encode(['success' => false]); // Return 'error' if GET data is missing
+// Check if the request method is GET
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+    exit;
 }
+
+// Get the type and brand parameters from the GET request
+$type = strtolower($_GET['type'] ?? '');
+$brand = strtolower($_GET['brand'] ?? '');
+
+// Check if the type parameter is missing
+if (empty($type)) {
+    http_response_code(400);
+    echo json_encode(['error' => 'No type sent yet!']);
+    exit;
+}
+
+// Check if the type parameter has less than 3 characters or above 10 characters
+if (strlen($type) < 3) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Type parameter has less than 3 characters']);
+    exit;
+} else if (strlen($type) > 10) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Type parameter has more than 10 characters']);
+    exit;
+}
+
+// Check if the type parameter contains non-alphabetical characters (except '-')
+if (!preg_match('/^[a-zA-Z\-]+$/', $type)) {
+    http_response_code(400);
+    echo json_encode(['error' => "this type has non-alphabetical characters (different from '-')."]);
+    exit;
+}
+
+// Check if the brand parameter is missing
+if (empty($brand)) {
+    http_response_code(400);
+    echo json_encode(['error' => 'No brand sent yet!']);
+    exit;
+}
+
+// Check if the brand parameter has less than 3 characters
+if (strlen($brand) < 3) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Brand parameter has less than 3 characters']);
+    exit;
+} else if (strlen($brand) > 20) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Brand parameter has more than 20 characters']);
+    exit;
+}
+
+// brand allows alphanumeric characters, - and &
+if (!preg_match('/^[a-zA-Z0-9\-&]+$/', $brand)) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Brand parameter has invalid characters']);
+    exit;}
+
+$query = <<<SQL
+
+SELECT * FROM products WHERE type = :type AND brand = :brand
+
+SQL;
+
+$statement = $conn->prepare($query);
+$statement->bindValue(':type', $type);
+$statement->bindValue(':brand', $brand);
+
+$statement->execute();
+
+if($statement->rowCount() !== 0) {
+    http_response_code(400);
+    echo json_encode(['error' => "$brand : This brand with $type already exists."]);
+    exit;
+}
+
+
+// If all checks pass, return a success message
+echo json_encode(['success' => true]);
+exit;
